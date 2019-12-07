9fa3e5a2924ee51b791a550c2de8c271b89d1df2
Kaede Fujisaki <kaede@hexe.net>
2019-12-07 22:06:35 +0900

    do not send to stderr

--- to-pixiv.sh
+++ to-pixiv.sh
@@ -8,4 +8,6 @@ cat "$1" | \
     sed ':a;N;$!ba;s/\n\{3,\}/\n\n/g' | \
     # https://unix.stackexchange.com/questions/552188/how-to-remove-empty-lines-from-begin-and-end-of-file
     sed -e '/./,$!d' -e :a -e '/^\n*$/{$d;N;ba' -e '}' \
-    > "$1.piviv.txt"
+    > "$1.pixiv.txt"
+
+echo "$1:" $(cat "$1.pixiv.txt" | LANG=en_US.UTF-8 wc -m) chars
