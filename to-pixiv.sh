2b48de0107128ee9c8a2e1cccd5619d3874778ab
Kaede Fujisaki <kaede@hexe.net>
2021-11-15 12:50:04 +0900

    Pixivやめた

--- to-pixiv.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-cat "$1" | \
-    sed -e :a -re 's/<!--.*?-->//g;/<!--/N;//ba' | \
-    sed -e 's/^\(\-\{3,\}\|\*\{3,\}\)/[newpage]/g' | \
-    sed -e 's/^\#.*//g' | \
-    sed ':a;N;$!ba;s/\n\{3,\}/\n\n/g' | \
-    # https://unix.stackexchange.com/questions/552188/how-to-remove-empty-lines-from-begin-and-end-of-file
-    sed -e '/./,$!d' -e :a -e '/^\n*$/{$d;N;ba' -e '}'
